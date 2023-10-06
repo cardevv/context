244ae49a0166bea5ec08ea28c9e2b9d81c9590ba
cardevv <113392365+cardevv@users.noreply.github.com>
2023-10-06 20:08:22 -0300

    iniciando estudo sobre react-router-dom e contextapi

---
+++ freelando/src/router/router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom"
+import SelecaoCliente from "../paginas/cadastro/SelecaoCliente";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+       
+        children: [
+        {
+
+            path:'cadastro',
+            element: <SelecaoCliente></SelecaoCliente>,
+            children: [
+
+
+                {
+
+                    path:'cliente',
+                    element: <h1>Interesses</h1>
+
+                },
+
+                {
+                    path:'dados-pessoais',
+                    element:<h1>Dados pessoais</h1>
+                },
+                {
+                    path:'concluido',
+                    element:<h1>Concluído</h1>
+
+                }
+            ]
+
+
+
+                }
+
+        ],
+    },
+]);
